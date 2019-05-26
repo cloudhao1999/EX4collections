7d9f61d94d54562e2632a091f796ad33a9e8a129
cloudhao1999 <cyh199910@outlook.com>
2019-05-26 22:20:58 +0800

    作业二

--- src/Student2.java
+++ src/Student2.java
@@ -12,6 +12,22 @@ public class Student2 {
 	        this.name = name;
 	        this.age = age;
 	    }
+	    public String getName() {
+	        return name;
+	    }
+
+	    public void setName(String name) {
+	        this.name = name;
+	    }
+
+	    public int getAge() {
+	        return age;
+	    }
+
+	    public void setAge(int age) {
+	        this.age = age;
+	    }
+
 	    @Override
 	    public boolean equals(Object s) {
 	    	if(this==s) return true;
@@ -45,10 +61,27 @@ public class Student2 {
 	        return result;
 	  
 	    }
+	    public String toString() {
+	        return "Student{" +
+	                " 姓名为'" + name + '\'' +
+	                ", 年龄为" + age +
+	                '}';
+	    }
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+		Map<Student2, String> map = new HashMap<>();
+        map.put(new Student2("傻子", 16), "C201");
+        map.put(new Student2("李四", 25), "C202");
+        map.put(new Student2("帅哥", 28), "C207");
 
+        for (Student2 student : map.keySet()) {
+            System.out.println(student + "地址:" + map.get(student));
+        }
+        System.out.println("---------------");
+        for (Map.Entry<Student2, String> next : map.entrySet()) {
+            System.out.println(next.getKey() + "地址:" + next.getValue());
+        }
 	}
 
 }
