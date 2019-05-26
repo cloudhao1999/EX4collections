c15ca2d66b5cc660a6961659ff77838f6062fcec
cloudhao1999 <cyh199910@outlook.com>
2019-05-26 21:49:31 +0800

    equal的重写

---
+++ src/Student.java
@@ -0,0 +1,39 @@
+import java.util.Iterator;
+import java.util.TreeSet;
+import java.util.Set;
+/*
+ * 1.创建学生类，包含学号、姓名、综合测评总分。使用TreeSet保存学生对象，按测评总分排序。
+ * 如果分数相同，按姓名排序。 最后再用迭代器输出学生对象。
+ */
+public class Student {
+	private int s_id;
+	private String name;
+    private double score;
+    public Student(int s_id, String name, double score) {
+        this.s_id = s_id;
+        this.name = name;
+        this.score = score;
+    }
+    @Override
+    public boolean equals(Object s) {
+    	if(this==s) return true;
+    	if(!(s instanceof Student)) return false;
+    	Student student=(Student)s;
+    	if(Double.compare(student.score, this.score)!=0) {
+    		return false;
+    	}
+    	if(this.s_id!=student.s_id) return false;
+    	boolean result=false;
+    	if(this.name!=null) {
+    		result=this.name.equals(student.name);
+    	}else {
+    		result=false;
+    	}
+    	return result;
+    }
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
